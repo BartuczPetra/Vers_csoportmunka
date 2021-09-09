ac029b316e82619422300f6cdd5921de5fb54eab
lejlailles <il970322@gmail.com>
2021-09-09 11:15:44 +0200

    index js folytatása: első feladat

---
+++ index.js
@@ -0,0 +1,19 @@
+
+ var versTomb=[
+    {
+        cim:"Csöndben ne lépj az éjszakába át",
+        szerzo:"Dylan Thomas",
+        vers:"",
+        kep:"",
+    }
+
+]
+
+$(document).ready(function(){
+ for (let index = 0; index < versTomb.length; index++) {
+     const element = versTomb[index];
+     $(".indexvers").append
+     
+ }
+ 
+  });
